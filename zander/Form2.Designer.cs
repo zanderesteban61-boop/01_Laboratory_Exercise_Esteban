d593a413eb9637bda9b9e0a1f9063684ce91c2c5
zanderesteban61-boop <zanderesteban61@gmail.com>
2025-08-15 22:49:44 +0800

    creating new form2

---
+++ zander/Form2.Designer.cs
@@ -0,0 +1,240 @@
+﻿namespace zander
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.labelStudent = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.labelProg = new System.Windows.Forms.Label();
+            this.labelLast = new System.Windows.Forms.Label();
+            this.labelFirst = new System.Windows.Forms.Label();
+            this.labelMiddle = new System.Windows.Forms.Label();
+            this.labelAge = new System.Windows.Forms.Label();
+            this.label13 = new System.Windows.Forms.Label();
+            this.labelContact = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // labelStudent
+            // 
+            this.labelStudent.AutoSize = true;
+            this.labelStudent.Font = new System.Drawing.Font("Microsoft YaHei", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelStudent.ImageAlign = System.Drawing.ContentAlignment.TopLeft;
+            this.labelStudent.Location = new System.Drawing.Point(27, 56);
+            this.labelStudent.Name = "labelStudent";
+            this.labelStudent.Size = new System.Drawing.Size(91, 19);
+            this.labelStudent.TabIndex = 0;
+            this.labelStudent.Text = "Student No. :";
+            this.labelStudent.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(295, 112);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 13);
+            this.label2.TabIndex = 1;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(168, 205);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(0, 13);
+            this.label3.TabIndex = 2;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(176, 213);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(0, 13);
+            this.label4.TabIndex = 3;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(175, 246);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(0, 13);
+            this.label5.TabIndex = 4;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(167, 272);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(0, 13);
+            this.label6.TabIndex = 5;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(175, 280);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(0, 13);
+            this.label7.TabIndex = 6;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(208, 245);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(0, 13);
+            this.label8.TabIndex = 7;
+            // 
+            // labelProg
+            // 
+            this.labelProg.AutoSize = true;
+            this.labelProg.Font = new System.Drawing.Font("Microsoft YaHei", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelProg.ImageAlign = System.Drawing.ContentAlignment.TopLeft;
+            this.labelProg.Location = new System.Drawing.Point(27, 92);
+            this.labelProg.Name = "labelProg";
+            this.labelProg.Size = new System.Drawing.Size(69, 19);
+            this.labelProg.TabIndex = 8;
+            this.labelProg.Text = "Program :";
+            // 
+            // labelLast
+            // 
+            this.labelLast.AutoSize = true;
+            this.labelLast.Font = new System.Drawing.Font("Microsoft YaHei", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelLast.ImageAlign = System.Drawing.ContentAlignment.TopLeft;
+            this.labelLast.Location = new System.Drawing.Point(27, 128);
+            this.labelLast.Name = "labelLast";
+            this.labelLast.Size = new System.Drawing.Size(82, 19);
+            this.labelLast.TabIndex = 9;
+            this.labelLast.Text = "Last Name :";
+            // 
+            // labelFirst
+            // 
+            this.labelFirst.AutoSize = true;
+            this.labelFirst.Font = new System.Drawing.Font("Microsoft YaHei", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelFirst.ImageAlign = System.Drawing.ContentAlignment.TopLeft;
+            this.labelFirst.Location = new System.Drawing.Point(27, 167);
+            this.labelFirst.Name = "labelFirst";
+            this.labelFirst.Size = new System.Drawing.Size(83, 19);
+            this.labelFirst.TabIndex = 10;
+            this.labelFirst.Text = "First Name :";
+            // 
+            // labelMiddle
+            // 
+            this.labelMiddle.AutoSize = true;
+            this.labelMiddle.Font = new System.Drawing.Font("Microsoft YaHei", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelMiddle.ImageAlign = System.Drawing.ContentAlignment.TopLeft;
+            this.labelMiddle.Location = new System.Drawing.Point(27, 205);
+            this.labelMiddle.Name = "labelMiddle";
+            this.labelMiddle.Size = new System.Drawing.Size(99, 19);
+            this.labelMiddle.TabIndex = 11;
+            this.labelMiddle.Text = "Middle Name :";
+            this.labelMiddle.Click += new System.EventHandler(this.label11_Click);
+            // 
+            // labelAge
+            // 
+            this.labelAge.AutoSize = true;
+            this.labelAge.Font = new System.Drawing.Font("Microsoft YaHei", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelAge.ImageAlign = System.Drawing.ContentAlignment.TopLeft;
+            this.labelAge.Location = new System.Drawing.Point(27, 239);
+            this.labelAge.Name = "labelAge";
+            this.labelAge.Size = new System.Drawing.Size(40, 19);
+            this.labelAge.TabIndex = 12;
+            this.labelAge.Text = "Age :";
+            // 
+            // label13
+            // 
+            this.label13.AutoSize = true;
+            this.label13.Font = new System.Drawing.Font("Microsoft YaHei", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label13.ImageAlign = System.Drawing.ContentAlignment.TopLeft;
+            this.label13.Location = new System.Drawing.Point(27, 312);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(65, 19);
+            this.label13.TabIndex = 13;
+            this.label13.Text = "Address :";
+            // 
+            // labelContact
+            // 
+            this.labelContact.AutoSize = true;
+            this.labelContact.Font = new System.Drawing.Font("Microsoft YaHei", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelContact.ImageAlign = System.Drawing.ContentAlignment.TopLeft;
+            this.labelContact.Location = new System.Drawing.Point(27, 274);
+            this.labelContact.Name = "labelContact";
+            this.labelContact.Size = new System.Drawing.Size(91, 19);
+            this.labelContact.TabIndex = 14;
+            this.labelContact.Text = "Contact No. :";
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Moccasin;
+            this.ClientSize = new System.Drawing.Size(371, 423);
+            this.Controls.Add(this.labelContact);
+            this.Controls.Add(this.label13);
+            this.Controls.Add(this.labelAge);
+            this.Controls.Add(this.labelMiddle);
+            this.Controls.Add(this.labelFirst);
+            this.Controls.Add(this.labelLast);
+            this.Controls.Add(this.labelProg);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.labelStudent);
+            this.Name = "Form2";
+            this.Load += new System.EventHandler(this.Form2_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labelStudent;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label labelProg;
+        private System.Windows.Forms.Label labelLast;
+        private System.Windows.Forms.Label labelFirst;
+        private System.Windows.Forms.Label labelMiddle;
+        private System.Windows.Forms.Label labelAge;
+        private System.Windows.Forms.Label label13;
+        private System.Windows.Forms.Label labelContact;
+    }
+}
